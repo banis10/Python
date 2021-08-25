f67edae1c6d75f9fe676421730aaeed07ca8c326
banis10r@gmail.com <banis@MacBook.local>
2021-08-25 17:02:05 -0600

    Ejemmplo de suma de conv 2.11

--- LabCom4/Practica2.py
+++ LabCom4/Practica2.py
@@ -55,10 +55,6 @@ plt.show()
 
 #Sumatoria de convolucion
 #Definir el tamaño de las muestras 
-
-
-
-#Definir el tamaño de las muestras 
 n = 9
 x = np.arange(-n,n,1)
 x0 = np.zeros(len(x))    #Entrada
@@ -87,3 +83,34 @@ plt.xlabel('n')
 plt.show()
 
 
+#Sumatoria de convolucion ejemplo del libro 2.11
+#Definir el tamaño de las muestras 
+n = 9
+x = np.arange(-n,n,1)
+x0 = np.zeros(len(x))    #Entrada
+h = np.arange(-n,n,1)   #Respuesta al impulso
+h0 = np.zeros(len(h))
+h0[n:n+4] = 1
+for i in x:
+    print(i)
+    if i >= 0:
+        x0[n+i]=pow(0.5,i)
+
+y = sp.signal.convolve(x0, h0,'same')
+yn = np.arange(-(n+1),n-1,1)
+#El inicio de y es la suma de x0 inicio y h0 inicio en este caso es n donde n es la posicion 0 
+plt.subplot(311)
+plt.ylabel('x[n]')
+plt.title('Sumatoria de convolución')
+plt.grid(True)
+plt.stem(x,x0)
+plt.subplot(312)
+plt.ylabel('h[n]')
+plt.grid(True)
+plt.stem(h,h0)
+plt.subplot(313)
+plt.stem(yn,y)
+plt.grid(True)
+plt.ylabel('y[n]')
+plt.xlabel('n')
+plt.show()
