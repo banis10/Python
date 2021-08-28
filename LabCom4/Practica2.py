a1e7154e0104e1d190775d5e08412464387574f0
banis10r@gmail.com <banis@MacBook.local>
2021-08-27 19:41:31 -0600

    Practica 2 terminada

--- LabCom4/Practica2.py
+++ LabCom4/Practica2.py
@@ -3,8 +3,10 @@ import numpy as np
 import scipy as sp
 from scipy import signal 
 import matplotlib.pyplot as plt
+
+#Interpolate
 t = np.arange(0,1,1/1000)
-x = np.sin(2*np.pi*30*t) + np.sin(2*np.pi*60*t)
+x = np.sin(2*np.pi*30*t)
 y = sp.signal.resample(x,len(x)*4)
 plt.subplot(211)
 plt.title('Funcion interpolate')
@@ -21,7 +23,7 @@ plt.show()
 
 #Decimate
 t = np.arange(0,1,1/4000)
-x = np.sin(2*np.pi*30*t) + np.sin(2*np.pi*60*t)
+x = np.sin(2*np.pi*30*t)
 y = sp.signal.decimate(x,4)
 plt.subplot(211)
 plt.title('Funcion decimate')
@@ -42,7 +44,7 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 t = np.arange(0,1,1/1000)
-x = np.sin(2*np.pi*30*t) + np.sin(2*np.pi*60*t)
+x = np.sin(2*np.pi*30*t)
 y = sp.signal.resample_poly(x,4,2)
 plt.subplot(211)
 plt.title('Funcion resample')
@@ -92,13 +94,17 @@ h = np.arange(-n,n,1)   #Respuesta al impulso
 h0 = np.zeros(len(h))
 h0[n:n+4] = 1
 for i in x:
-    print(i)
     if i >= 0:
         x0[n+i]=pow(0.5,i)
 
 y = sp.signal.convolve(x0, h0,'same')
 yn = np.arange(-(n+1),n-1,1)
+
+u = np.ones(len(y))
+un = sp.signal.convolve(u,y,'same')
+
 #El inicio de y es la suma de x0 inicio y h0 inicio en este caso es n donde n es la posicion 0 
+
 plt.subplot(311)
 plt.ylabel('x[n]')
 plt.title('Sumatoria de convolución')
@@ -114,3 +120,130 @@ plt.grid(True)
 plt.ylabel('y[n]')
 plt.xlabel('n')
 plt.show()
+
+
+#Respuesta al impulso
+n = 9
+x = np.arange(-n,n,1)
+x0 = np.zeros(len(x))    #Entrada
+h = np.arange(-n,n,1)   #Respuesta al impulso
+h0 = np.zeros(len(h))
+h0[n:n+4] = 1
+for i in x:
+    if i >= 0:
+        x0[n+i]=pow(0.5,i)
+y = sp.signal.convolve(x0, h0,'same')
+yn = np.arange(-(n+1),n-1,1)
+
+u = np.ones(len(y))
+
+un = sp.signal.convolve(u,y)
+u0 = np.arange(0,len(un),1)
+plt.subplot(311)
+plt.ylabel('x[n]')
+plt.title('Respuesta al escalon')
+plt.grid(True)
+plt.stem(yn,u)
+plt.subplot(312)
+plt.ylabel('h[n]')
+plt.grid(True)
+plt.stem(yn,y)
+plt.subplot(313)
+plt.stem(u0,un)
+plt.grid(True)
+plt.ylabel('y[n]')
+plt.xlabel('n')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''MUESTREO: Genere una señal senoidal en tiempo contínuo x(t) de frecuencia f0 = 300 Hz
+y fase 0°x(t) utilizadno un vector t de valores de tiempo con intervalos de muestreo de 
+10 mS (0.010). Muestree la señal x(t) para obtener una secuencia x[n] en tiempo 
+discreto haciendo x[n] = x(t=nTs) siendo Ts = 1/fs segundos. Utilice una frecuencia de 
+muestreo fs= 8KHz. Grafique x(t) usando la función Plot y grafique x[n] con la función 
+Stem presentándolas en la misma pantalla. Posteriormente genere señales x(t) con los 
+siguientes valores de f0 y muestreelas con fs = 8KHz:
+
+a. Desde 100 Hz hasta 475 Hz en saltos de 125 Hz. Grafíque cada x(t) y cada una las
+secuencias resultantes en una misma pantalla.
+
+b. Desde 7,525 hasta 7,900 Hz en saltos de 125 Hz. Grafíque cada x(t) y cada una las
+secuencias resultantes en una misma pantalla.'''
+
+#w=2pi*f
+
+fo = 300
+fs = 8000
+fo1 = 100
+fo2 = 125
+fo3 = 250
+fo4 = 
+n = np.arange(0,1,1/100)
+t = np.arange(0,1,1/100)
+x = np.sin(2*np.pi*fo/fs*t)
+x = list()
+for i,c in range(100,600,125):
+    print(i)
+    print(x)
+plt.subplot(311)
+plt.stem(n,x,)
+plt.ylabel('x[n]')
+plt.xlabel('n')
+plt.grid(True)
+
+plt.subplot(312)
+plt.plot(t,x)
+plt.ylabel('x[n]')
+plt.xlabel('n')
+plt.grid(True)
+
+plt.subplot(313)
+plt.subplot 
+plt.show()
+
+
