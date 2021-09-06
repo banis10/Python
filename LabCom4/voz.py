4761b7898259e37da19fdcb49b62dac86afe50ff
banis10r@gmail.com <banis@MacBook.local>
2021-09-05 18:32:33 -0600

    Reconocimiento de voz

---
+++ LabCom4/voz.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr 
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print('Say something...')
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print('What did you say : {}'.format(text))
+    except:
+        print('Sorry! I can\'t understand') 
