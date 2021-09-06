ddb524778bd2005557eea835621d1306f56a1186
banis10r@gmail.com <banis@MacBook.local>
2021-09-05 18:41:37 -0600

    Lenguaje español

--- LabCom4/voz.py
+++ LabCom4/voz.py
@@ -4,7 +4,7 @@ with sr.Microphone() as source:
     print('Say something...')
     audio = r.listen(source)
     try:
-        text = r.recognize_google(audio)
+        text = r.recognize_google(audio, language="es-ES")
         print('What did you say : {}'.format(text))
     except:
         print('Sorry! I can\'t understand') 
