5ee01bb6284208f5e288123d44f4399c14a48d66
banis10r@gmail.com <banis@MacBook.local>
2021-09-09 14:09:23 -0600

    Practica 3

--- LabCom4/Practica3.py
+++ LabCom4/Practica3.py
@@ -1,5 +1,15 @@
 #Practica 3 
+'''En ésta práctica se realiza lo siguiente: Se determina la transformada de Fourier en tiempo 
+discreto de una secuencia h[n] a partir de su expresión calculada analíticamente. La 
+transformada discreta de Fourier utilizando el algoritmo de la trasformada rápida de Fourier
+fft de una señal que es el resultado de la suma de dos señales senoidales corrompida con
+ruido aleatorio de media 0. Se calculan los valores máximos de la auto correlación de una 
+señal senoidal X y la de la correlación cruzada entre dos señales senoidales X e Y con 
+distintos valores de frecuencia. Utilizando la función filter se determina la respuesta al 
+impulso h[n] de una ecuación en diferencias recursiva causal.'''
 
+
+ 
 '''1. TRANSFORMADA DE FOURIER EN TIEMPO DISCRETO TFTD: Genere un vector respuesta 
 al impulso h[n] con un pulso de longitud de L muestras y amplitud 1 y un vector N con 
 los valores de frecuencias a evaluar entre (– π, π ) con la misma longitud del vector h[n].
@@ -8,12 +18,29 @@ Fourier en tiempo discreto de la respuesta al impulso. Grafique h[n] y H en la m
 pantalla.'''
 
 
+
+
 '''2. TRANSFORMADA DISCRETA DE FOURIER TDF Y TRANSFORMADA RÁPIDA DE FOURIER 
 FFT: Genere una señal compuesta como la suma de dos señales senoidales con 
 amplitudes de 1 y 0.8 con frecuencias de 100 y 300 Hz respectivamente. Agregue ruido 
 aleatorio de media cero a dicha señal. Obtenga la TDF utilizando la función fft. Grafique 
 la señal generada y la TDF de dicha señal.'''
-
+from scipy.fft import fft, fftfreq
+import numpy as np
+import matplotlib.pyplot as plt
+#Number of sample points
+N = 600
+# sample spacing
+T = 1.0/ 800.0
+x = np.linspace(0.0, N*T, N, endpoint = False)
+y = np.sin(100*2*np.pi*x) + 0.8*np.sin(300*2*np.pi*x)
+rng = np.random.default_rng()
+sig_noise = y + rng.standard_normal(len(y))
+yf = fft(sig_noise)
+xf = fftfreq(N, T)[:N//2]
+plt.plot(xf,2/N* np.abs(yf[0:N//2]))
+plt.grid()
+plt.show()
 
 
 '''3. AUTO CORRELACIÓN Y CORRELACIÓN CRUZADA: Genere una secuencia X que sea una 
