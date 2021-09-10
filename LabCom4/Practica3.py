34be149df2f2a67d58a8c0176c771c46d5caa08a
banis10r@gmail.com <banis@MacBook.local>
2021-09-10 16:43:31 -0600

    Practica 3 finalizada

--- LabCom4/Practica3.py
+++ LabCom4/Practica3.py
@@ -16,10 +16,59 @@ los valores de frecuencias a evaluar entre (– π, π ) con la misma longitud d
 Calcule la TFTD como un vector H utilizando la expresión analítica de la transformada de 
 Fourier en tiempo discreto de la respuesta al impulso. Grafique h[n] y H en la misma 
 pantalla.'''
+import numpy as np
+from scipy.fft import fft, ifft
+from sympy import symbols,Function, Sum
+import matplotlib.pyplot as plt
+from math import *
+#X(e^jw)= sum(x[n]*e^-jwn)o
+x = np.ones(7)
+H = np.zeors(7)
+w = symbols('w')
+for c in x:
+    H[0] = e**w
+
+f = Function('x[n]*e**(-1j*n)')
+H=Sum(f(n),(n,0,7)).doit()
+
+n=20
+wn = np.arange(0,n,1)
+w = np.linspace(-pi,pi,n)
+H = 1+2*np.cos(w)
+#Respuesta 
+plt.subplot(211)
+plt.grid()
+plt.title('Respuesta en frecuencia')
+plt.plot(w,H)
+#Modulo
+plt.subplot(212)
+plt.title('Modulo')
+mod = abs(H)
+plt.grid()
+plt.plot(w,mod)
+plt.show()
 
 
-
-
+n=50
+w = np.linspace(-pi,pi,n)
+hn = np.ones(7)
+h0 = np.arange(0,7,1)
+H = 1+e**(-1j*w)+e**(-2j*w)+e**(-3j*w)+e**(-4j*w)+e**(-5j*w)+e**(-6j*w)
+#Escalon
+plt.subplot(311)
+plt.stem(h0,hn)
+#Respuesta 
+plt.subplot(312)
+plt.title('Respuesta en Frecuencia')
+plt.plot(w,np.real(H))
+plt.grid()
+#Modulo
+plt.subplot(313)
+plt.title('Modulo')
+mod = abs(H)
+plt.grid()
+plt.plot(w,mod)
+plt.show()
 '''2. TRANSFORMADA DISCRETA DE FOURIER TDF Y TRANSFORMADA RÁPIDA DE FOURIER 
 FFT: Genere una señal compuesta como la suma de dos señales senoidales con 
 amplitudes de 1 y 0.8 con frecuencias de 100 y 300 Hz respectivamente. Agregue ruido 
@@ -28,9 +77,7 @@ la señal generada y la TDF de dicha señal.'''
 from scipy.fft import fft, fftfreq
 import numpy as np
 import matplotlib.pyplot as plt
-#Number of sample points
 N = 600
-# sample spacing
 T = 1.0/ 800.0
 x = np.linspace(0.0, N*T, N, endpoint = False)
 y = np.sin(100*2*np.pi*x) + 0.8*np.sin(300*2*np.pi*x)
@@ -49,11 +96,36 @@ función auto correlación y muestre el coeficiente máximo. Ahora genere otra s
 Y que sea senoidal muestreada a 10 mS de amplitud 1, de frecuencia F con 100 ≤ F ≤ 400 
 Hz en saltos de 100 Hz, aplique la función correlación cruzada a X e Y para cada 
 frecuencia. Muestre los coeficientes máximos de cada caso.'''
+from scipy import signal
+N = 1000
+t = np.arange(0,1,1/N)
+y = np.sin(2*np.pi*100*t)
+y1 = np.sin(200*2*np.pi*t)
+y2 = np.sin(300*2*np.pi*t)
+y3 = np.sin(400*2*np.pi*t)
 
+c = signal.correlate(y,y,mode='full')/N
+c1 = signal.correlate(y,y1)/N
+c2 = signal.correlate(y,y2)/N
+c3 = signal.correlate(y,y3)/N
+
+m = max(c)
+m1 = max(c)
+m2 = max(c1)
+m3 = max(c3)
 '''4. ECUACIONES EN DIFERENCIAS: La función –filter- implementa un filtro digital caracterizado 
-por los coeficientes a y b de una ecuación en diferencias recursiva. Entonces y= filter(b,a,x)
-calcula la salida del filtro debida a las entradas que están en el vector x. Sí x es la secuencia 
-impulso δ[n] entonces y[n] = h[n]. Para la ecuación en diferencias causal y[n] – 1.143y[n-1] 
-+ 0.4128y[n-2] = 0.0675x[n] + 0.1349x[n-1] + 0.0675x[n-2] determine su respuesta impulso
-h[n] creando un vector x = δ[n] de longitud 50. Genere los primeros 50 puntos de h[n] y 
-grafique las primeras 20 muestras.'''
+por los coeficientes a y b de una ecuación en diferencias recursiva. 
+Entonces y= filter(b,a,x) calcula la salida del filtro debida a las entradas que están en el vector x.
+Sí x es la secuencia impulso δ[n] entonces y[n] = h[n]. 
+Para la ecuación en diferencias causal
+y[n] – 1.143y[n-1]+ 0.4128y[n-2] = 0.0675x[n] + 0.1349x[n-1] + 0.0675x[n-2] 
+determine su respuesta impulso h[n] creando un vector x = δ[n] de longitud 50. Genere los primeros 50 puntos de h[n] y grafique las primeras 20 muestras.'''
+x = np.ones(50)
+n = np.arange(0,len(x),1)
+a = np.array([1,-1.143,0.4128 ])
+b = np.array([0.0675,0.1349,0.0675 ])
+
+h = signal.lfilter(b,a,x)
+
+plt.plot(n[:20],h[:20])
+plt.show()
