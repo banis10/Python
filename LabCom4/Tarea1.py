c022feed49dfb0a0fdca3a7fecc1d9e841f4abc5
banis10r@gmail.com <banis@MacBook.local>
2021-09-20 13:31:49 -0600

    Tarea com 4

--- LabCom4/Tarea1.py
+++ LabCom4/Tarea1.py
@@ -137,3 +137,76 @@ fase =np.arctan(im/re)
 plt.plot(w,fase)
 plt.show()
 
+#Problema 2.67
+
+n = 9
+x = np.arange(-n,n,1)
+x0 = np.zeros(len(x))    #Entrada
+h = np.arange(-n,n,1)   #Respuesta al impulso
+h0 = np.zeros(len(h))
+x0[n] = 1
+x0[n+1]=-1
+x0[n+2]=-2
+x0[n+3]=2
+x0[n+4]=1
+x0[n+5]=-1
+
+h0[n] = 1
+h0[n+5]=-1
+
+y = sp.signal.convolve(x0, h0,'same')
+yn = np.arange(-(n+1),n-1,1)
+#El inicio de y es la suma de x0 inicio y h0 inicio en este caso es n donde n es la posicion 0 
+plt.subplot(311)
+plt.ylabel('x[n]')
+plt.title('Sumatoria de convolución')
+plt.grid(True)
+plt.stem(x,x0)
+plt.subplot(312)
+plt.ylabel('h[n]')
+plt.grid(True)
+plt.stem(h,h0)
+plt.subplot(313)
+plt.stem(yn,y)
+plt.grid(True)
+plt.ylabel('y[n]')
+plt.xlabel('n')
+plt.show()
+
+
+#problema 2.67
+
+n = 9
+x = np.arange(-n,n,1)
+x0 = np.zeros(len(x))    #Entrada
+h = np.arange(-n,n,1)   #Respuesta al impulso
+h0 = np.zeros(len(h))
+x0[n] =1
+x0[n+1]=-1
+
+h0[n] = 1
+h0[n+1]=1
+h0[n+2]=-1
+h0[n+3]=-1
+
+y = sp.signal.convolve(x0, h0,'same')
+yn = np.arange(-(n+1),n-1,1)
+
+y2 =sp.signal.convolve(y,x0,'same')
+
+#El inicio de y es la suma de x0 inicio y h0 inicio en este caso es n donde n es la posicion 0 
+plt.subplot(311)
+plt.ylabel('x[n]')
+plt.title('Sumatoria de convolución')
+plt.grid(True)
+plt.stem(x,x0)
+plt.subplot(312)
+plt.ylabel('h[n]')
+plt.grid(True)
+plt.stem(h,h0)
+plt.subplot(313)
+plt.stem(yn,y)
+plt.grid(True)
+plt.ylabel('y[n]')
+plt.xlabel('n')
+plt.show()
