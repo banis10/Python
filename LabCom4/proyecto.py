a44118a27bc97263a43a9f7d615d84a34e255695
banis10r@gmail.com <banis@MacBook.local>
2021-09-20 23:59:54 -0600

    Proyecto com 4

--- LabCom4/proyecto.py
+++ LabCom4/proyecto.py
@@ -1,13 +1,14 @@
 import pyttsx3
 import speech_recognition as sr
+import time
 engine = pyttsx3.init()
 #Control the rate. Higher rate = more speed
 engine.setProperty("rate", 150)
-text = "Buenos dias, para que dosis de vacuna viene"
+text = "Buenos dias,por favor coloque su DPI"
 engine.say(text)
-"""
-Queue another audio
-"""
+engine.runAndWait()
+text = "Para que dosis de vacuna viene"
+engine.say(text)
 engine.runAndWait()
 
 r = sr.Recognizer()
@@ -15,21 +16,23 @@ with sr.Microphone() as source:
     print('Say something...')
     audio = r.listen(source)
     try:
-        text = r.recognize_google(audio, language = 'es-ES')
-        print('What did you say : {}'.format(text))
+        rec = r.recognize_google(audio, language = 'es-ES')
+        print('What did you say : {}'.format(rec))
     except:
         print('Sorry! I can\'t understand')
         
 
-text1 = "Viene a la primera dosis, por favor dirijase al módulo A"
-engine.say(text1)
-"""
-Queue another audio
-"""
-engine.runAndWait()
-text2 = "Viene a la segunda dosis, por favor dirijase al módulo B"
-engine.say(text2)
-"""
-Queue another audio
-"""
-engine.runAndWait()
+vac = rec.split()
+print(vac)
+
+if 'primera' in vac:
+    text1 = "Viene a la primera dosis, por favor dirijase al módulo A"
+    engine.say(text1)
+    engine.runAndWait()
+if 'segunda' in vac:
+    engine.runAndWait()
+    text2 = "Viene a la segunda dosis, por favor dirijase al módulo B"
+    engine.say(text2)
+    engine.runAndWait()
+
+
