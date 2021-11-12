a582630ed41abda58d2f5cd5c73e1a17f2af8886
banis <banis10r@gmail.com>
2021-11-12 14:28:10 -0600

    banner para redes

--- LabCom4/fp.py
+++ LabCom4/fp.py
@@ -6,7 +6,6 @@ y=(x+1)/(x**2-2*x+1)
 apart(y)
   
 #Ejemplo de la clase
-a=(1 - 2*z**-1 + z**-2)
 a=(1+z**-1)**2
 b=( 1 - (3/2)*z**-1 + (1/2)*z**-2)
 Xz=a/b
@@ -15,5 +14,13 @@ Xz=a/b
 
 a=x**2+2*x-1
 b=2*x**3+3*x**2-2*x
+Xz=a/b
+apart(Xz)
+
+
+
+
+a=(7*x+23)
+b=(x+5)*(x+2)
 Xz=a/b
 apart(Xz)
